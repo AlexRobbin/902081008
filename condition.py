e5f6c07c25401d3867a639a45d6999a31f194c4e
AlexRobbin <158841503+AlexRobbin@users.noreply.github.com>
2024-02-07 21:13:27 +0330

    fourth day, condition, is,comparison operator

---
+++ condition.py
@@ -0,0 +1,73 @@
+#.................condition.................abs
+'''
+print(" enter a orders :")
+x = input()
+
+
+if x == "elevator":
+  print("up and down")
+elif x == "aileron":
+    print("right and left")
+elif x == "bldc":
+    print("start the engine")
+else: 
+    print("autopilot")
+'''
+#.................end.....................
+
+#.................in a line...............................
+#print("autopilot") if x == "autopilot"  else print("get a orders")
+#.................end.....................
+'''
+x = ["a" , "b" , "c"]
+y = ["a" , "b" , "c"]
+print( x == y)   # >>>>>>> TRUE
+print(x is y)    # >>>>>>> FALSE
+
+
+x = 22
+y = 22
+print( x == y)   # >>>>>>> TRUE
+print(x is y)    # >>>>>>> TRUE
+'''
+
+
+#...........comparisone................
+# .........==............
+
+'''
+num_1 = 5
+num_2 = 6
+print(f" num_1 == num_2 is :{ num_1 == num_2 }")
+'''
+#.................end.....................
+
+
+
+# .........!=............
+'''
+num_1 = 5
+num_2 = 6
+print(f" num_1 != num_2 is :{ num_1 != num_2 }")
+'''
+#.................end.....................
+
+
+
+# .........greater than(>)............
+'''
+num_1 = 5
+num_2 = 6
+print(f" num_1 > num_2 is :{ num_1 > num_2 }")
+'''
+#.................end.....................
+
+
+
+# .........less than(<)............
+'''
+num_1 = 5
+num_2 = 6
+print(f" num_1 < num_2 is :{ num_1 < num_2 }")
+'''
+#.................end.....................
